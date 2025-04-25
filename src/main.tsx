c47892ec406544760d1de4702b5f004c0a751e0b
zagorky <zagorkiynahyi@gmail.com>
2025-04-25 13:40:24 +0300

    refactor: remove unused styles, favicon, add temporary nonNullable helper

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {assertIsNonNullable} from "~components/shared/helpers.ts";
 
 const rootElement = document.querySelector('#root');
+assertIsNonNullable(rootElement);
 
 createRoot(rootElement).render(
   <StrictMode>
