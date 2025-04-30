0d871adae2192397b556c45cc71ae741301c1aba
Anastasia Savrukhina <assavr0@gmail.com>
2025-04-30 12:25:29 +0300

    chore(RSS-ECOMM-1_12): remove commit message pattern and move hint to commit-msg

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-  },
-  plugins: [
-    {
-      rules: {
-        'subject-pattern': ({ subject }) => {
-          const pattern = /^RSS-ECOMM-\d{1,2}_\d{1,2}:\s[a-zA-Z0-9\s]+$/;
-          const isValid = pattern.test(subject || '');
-          return [
-            isValid,
-            '🐶 Commit message must look like: `chore(RSS-ECOMM-1_10): short description`',
-          ];
-        },
-      },
-    },
-  ],
-  rulesConfig: {
-    'subject-pattern': [2, 'always'],
   },
 };
