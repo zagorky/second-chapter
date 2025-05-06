b1804038c845d8447c0949dd3cb6f49831c6a457
zagorky <zagorkiynahyi@gmail.com>
2025-05-06 18:59:28 +0300

    feat: add links to all the functional pages of the application on the main page

--- src/app/pages/main-page/mainPage.tsx
+++ src/app/pages/main-page/mainPage.tsx
@@ -1,7 +1,28 @@
+import { Button } from '~components/ui/button/button.tsx';
+import { NAVIGATION_ROUTES } from '~config/navigation.ts';
+import { Link } from 'react-router';
+
 const MainPage = () => {
   return (
     <>
       <h1 className={'heading-1'}>Second Chapter Store</h1>
+      <div className="flex items-center justify-center gap-3">
+        <Button variant="outline" className="hidden sm:inline-flex" asChild>
+          <Link to={NAVIGATION_ROUTES.about}>About us</Link>
+        </Button>
+        <Button variant="outline" className="hidden sm:inline-flex" asChild>
+          <Link to={NAVIGATION_ROUTES.catalog}>Catalog</Link>
+        </Button>
+        <Button variant="outline" className="hidden sm:inline-flex" asChild>
+          <Link to={NAVIGATION_ROUTES.cart}>Cart</Link>
+        </Button>
+        <Button variant="outline" className="hidden sm:inline-flex" asChild>
+          <Link to={NAVIGATION_ROUTES.signin}>Sign In</Link>
+        </Button>
+        <Button variant="outline" className="hidden sm:inline-flex" asChild>
+          <Link to={NAVIGATION_ROUTES.signup}>Sign Up</Link>
+        </Button>
+      </div>
     </>
   );
 };
