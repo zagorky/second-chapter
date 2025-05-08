9d90cc2dd5d563cc95fbe715caf8dac4fa6d41d6
Anastasia Savrukhina <123760669+savryna@users.noreply.github.com>
2025-05-08 09:52:01 +0300

    chore: fix development environment configuration
    * chore: set up ESLint configuration, added Vitest coverage support, and created test for SignInPage
    * chore(RSS-ECOMM-1_19): add description for test:coverage script vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,16 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['node_modules/**'],
+      all: true,
+    },
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
   },
 });
