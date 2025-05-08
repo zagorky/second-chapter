65b33fcf1edc988b2ead6d996066900304b84ad0
zagorky <zagorkiynahyi@gmail.com>
2025-05-08 21:26:07 +0300

    refactor(main-layout): add key to outlet

--- src/components/layouts/mainLayout.tsx
+++ src/components/layouts/mainLayout.tsx
@@ -1,12 +1,14 @@
 import { Header } from '~components/ui/header/header';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 
 export const MainLayout = () => {
+  const location = useLocation();
+
   return (
     <div className="bg-muted min-h-screen">
       <Header />
       <main className="mx-auto w-full max-w-6xl p-8 text-center">
-        <Outlet />
+        <Outlet key={location.pathname} />
       </main>
     </div>
   );
