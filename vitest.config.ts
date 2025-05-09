26271d2678088e25662df92454a8e057baf6cf00
zagorky <zagorkiynahyi@gmail.com>
2025-05-09 11:26:22 +0300

    refactor(vitest config): add merging vite config and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vitest/config';
+import config from './vite.config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './vitest.setupTests.ts',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      reportsDirectory: './coverage',
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: ['node_modules/**'],
-      all: true,
+export default mergeConfig(
+  config,
+  defineConfig({
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './vitest.setupTests.ts',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        reportsDirectory: './coverage',
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: ['node_modules/**'],
+        all: true,
+      },
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+      },
     },
-    alias: {
-      '~': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  })
+);
