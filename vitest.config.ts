97a2a4703618c818b3dcb6451e3d1c069c1d2f39
zagorky <zagorkiynahyi@gmail.com>
2025-05-10 11:37:28 +0300

    refactor: add config changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from 'vitest/config';
-import { mergeConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 import config from './vite.config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
