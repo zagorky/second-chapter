5818effd30acd451905f70a16abefa8420872d11
zagorky <zagorkiynahyi@gmail.com>
2025-05-10 22:28:42 +0300

    feat: add conditional rendering to buttons and redirecting to signin page after logout

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -4,10 +4,13 @@ import { NavigationSheet } from '~components/ui/navbar/navigationSheet';
 import { NavMenu } from '~components/ui/navbar/navMenu';
 import { navigationRoutes } from '~config/navigation';
 import { LogoutButton } from '~features/logout/components/logoutButton';
+import { useAppStore } from '~stores/store';
 import { BookOpen, LogIn, UserPlus } from 'lucide-react';
 import { Link } from 'react-router';
 
 export const Header = () => {
+  const isAuthenticated = useAppStore((state) => state.isAuthenticated);
+
   return (
     <header className="bg-background mx-auto flex h-16 max-w-screen-xl items-center justify-between border-b px-4 sm:px-6 lg:px-8">
       <div className="flex items-center gap-8">
@@ -20,18 +23,21 @@ export const Header = () => {
 
       <div className="flex items-center gap-3">
         <Button variant="outline" className="sm:inline-flex" asChild>
-          <Link to={navigationRoutes.login.path}>
-            <LogIn className="size-4 sm:hidden sm:size-0" />
-            <span className="hidden sm:inline">{navigationRoutes.login.title}</span>
-          </Link>
-        </Button>
-        <Button variant="outline" className="sm:inline-flex" asChild>
           <Link to={navigationRoutes.signup.path} className="flex items-center gap-2">
             <UserPlus className="size-4 sm:hidden sm:size-0" />
             <span className="hidden sm:inline">{navigationRoutes.signup.title}</span>
           </Link>
         </Button>
-        <LogoutButton />
+        {isAuthenticated ? (
+          <LogoutButton />
+        ) : (
+          <Button variant="outline" className="sm:inline-flex" disabled={!isAuthenticated} asChild>
+            <Link to={navigationRoutes.login.path}>
+              <LogIn className="size-4 sm:hidden sm:size-0" />
+              <span className="hidden sm:inline">{navigationRoutes.login.title}</span>
+            </Link>
+          </Button>
+        )}
         <ModeToggle />
         <div className="md:hidden">
           <NavigationSheet />
--- src/features/logout/components/logoutButton.tsx
+++ src/features/logout/components/logoutButton.tsx
@@ -1,11 +1,16 @@
 import { Button } from '~components/ui/button/button';
+import { navigationRoutes } from '~config/navigation';
 import { useAuth } from '~features/sign-in/hooks/useAuth';
 import { LogOut } from 'lucide-react';
+import { useNavigate } from 'react-router';
 
 export const LogoutButton = () => {
   const { logout } = useAuth();
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     logout();
+    void navigate(navigationRoutes.login.path);
   };
 
   return (
