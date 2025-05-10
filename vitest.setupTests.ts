0a746ecdf49d04a7cef22584353932ad217c5245
Anastasiia Nikonova <anastaney@gmail.com>
2025-05-10 09:11:46 +0200

    fix: move test import.meta.env definition to vitest.setupTests.ts

--- vitest.setupTests.ts
+++ vitest.setupTests.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom/vitest';
+
+Object.defineProperty(import.meta, 'env', {
+  value: {
+    VITE_CTP_CLIENT_ID: 'test-id',
+    VITE_CTP_CLIENT_SECRET: 'secret',
+    VITE_CTP_SCOPES: 'scope scope',
+    VITE_CTP_PROJECT_KEY: 'key',
+    VITE_CTP_API_URL: 'url',
+    VITE_CTP_AUTH_URL: 'url',
+  },
+});
