f6a86e8a87827528ac4c6317107d06e1f6f6064d
zagorky <zagorkiynahyi@gmail.com>
2025-05-11 10:29:49 +0300

    refactor: edit a little logout button

--- src/features/logout/components/logoutButton.tsx
+++ src/features/logout/components/logoutButton.tsx
@@ -2,23 +2,21 @@ import { Button } from '~components/ui/button/button';
 import { navigationRoutes } from '~config/navigation';
 import { useAuth } from '~features/sign-in/hooks/useAuth';
 import { LogOut } from 'lucide-react';
-import { useNavigate } from 'react-router';
+import { Link } from 'react-router';
 
 export const LogoutButton = () => {
   const { logout } = useAuth();
-  const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
-    void navigate(navigationRoutes.login.path);
   };
 
   return (
     <Button variant="outline" className="sm:inline-flex" onClick={handleLogout}>
-      <div className="flex items-center gap-2">
+      <Link to={navigationRoutes.login.path} className="flex items-center gap-2">
         <LogOut className="size-4 sm:hidden sm:size-0" />
         <span className="hidden sm:inline">Logout</span>
-      </div>
+      </Link>
     </Button>
   );
 };
