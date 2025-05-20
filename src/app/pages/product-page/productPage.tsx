fb53750f290713a2e10014e1744e082848089788
zagorky <zagorkiynahyi@gmail.com>
2025-05-20 21:41:24 +0300

    feat: add product page routing

---
+++ src/app/pages/product-page/productPage.tsx
@@ -0,0 +1,24 @@
+import type { ProductProjection } from '@commercetools/platform-sdk';
+
+import { Button } from '~components/ui/button/button';
+import { Link } from 'react-router';
+
+type ProductPageProps = {
+  product: ProductProjection;
+};
+
+const ProductPage = ({ product }: ProductPageProps) => {
+  return (
+    <div>
+      <h1 className={'heading-1'}>Product Page</h1>
+      {product}
+      <Button variant="default" className="inline-flex">
+        <Link to=".." relative="path">
+          Go back
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default ProductPage;
