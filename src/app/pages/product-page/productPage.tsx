aab7307ea33c7acbb1e32cad5ce2be73dbbf5e82
zagorky <zagorkiynahyi@gmail.com>
2025-05-20 22:40:52 +0300

    fix: remove product from product page

--- src/app/pages/product-page/productPage.tsx
+++ src/app/pages/product-page/productPage.tsx
@@ -1,17 +1,10 @@
-import type { ProductProjection } from '@commercetools/platform-sdk';
-
 import { Button } from '~components/ui/button/button';
 import { Link } from 'react-router';
 
-type ProductPageProps = {
-  product: ProductProjection;
-};
-
-const ProductPage = ({ product }: ProductPageProps) => {
+const ProductPage = () => {
   return (
     <div>
       <h1 className={'heading-1'}>Product Page</h1>
-      {product}
       <Button variant="default" className="inline-flex" asChild>
         <Link to=".." relative="path">
           Go back
