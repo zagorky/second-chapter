8a65afd840e56b8a158303605ee55d7e3b0398a6
zagorky <zagorkiynahyi@gmail.com>
2025-05-21 22:14:54 +0300

    refactor: remove changes from error fallback elem

--- src/components/ui/error-fallback/errorFallback.tsx
+++ src/components/ui/error-fallback/errorFallback.tsx
@@ -3,30 +3,20 @@ import type { FallbackProps } from 'react-error-boundary';
 import { Button } from '~/components/ui/button/button';
 import { Card, CardFooter, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
 
-type ErrorFallbackProps = {
-  title?: string;
-  retryText?: string;
-} & FallbackProps;
-
-export const ErrorFallback = ({
-  error,
-  resetErrorBoundary,
-  title = 'Something went wrong',
-  retryText = 'Try once more',
-}: ErrorFallbackProps) => {
+export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   const message = error instanceof Error ? error.message : 'Unknown error';
 
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Card className="">
         <CardHeader>
-          <CardTitle>{title}</CardTitle>
+          <CardTitle>Something went wrong</CardTitle>
         </CardHeader>
         <CardContent>
           <p>{message}</p>
         </CardContent>
         <CardFooter>
-          <Button onClick={resetErrorBoundary}>{retryText}</Button>
+          <Button onClick={resetErrorBoundary}>Try once more</Button>
         </CardFooter>
       </Card>
     </div>
