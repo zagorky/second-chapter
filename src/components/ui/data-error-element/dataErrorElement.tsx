03716a46db9d78b1b56f5353d0f759f43d3e8563
zagorky <zagorkiynahyi@gmail.com>
2025-05-22 14:20:25 +0300

    refactor: add tests and remove empty className

--- src/components/ui/data-error-element/dataErrorElement.tsx
+++ src/components/ui/data-error-element/dataErrorElement.tsx
@@ -16,7 +16,7 @@ export const DataErrorElement = ({
 }: DataErrorElementProps) => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Card className="">
+      <Card>
         <CardHeader>
           <CardTitle>{title}</CardTitle>
         </CardHeader>
