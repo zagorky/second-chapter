a40e73a532eed61343be0c557f08fe2055390d47
zagorky <zagorkiynahyi@gmail.com>
2025-05-22 13:53:41 +0300

    refactor: add an empty list component to render in case there are no products

---
+++ src/features/fetch-products/components/emptyList.tsx
@@ -0,0 +1,22 @@
+import { Button } from '~components/ui/button/button';
+import { navigationRoutes } from '~config/navigation';
+import { Link } from 'react-router';
+
+export const EmptyList = () => {
+  return (
+    <div className="flex h-[calc(100vh-200px)] flex-col items-center justify-center">
+      <div className="m-auto max-w-xl p-10">
+        <h3>Fear not, intrepid reader!</h3>
+        <p>
+          Our shelves are currently empty. We&#39;re working to restock our collection of pre-loved books. Try
+          refreshing the page or visit us again soon!
+        </p>
+      </div>
+      <Button variant="default" className="sm:inline-flex" asChild>
+        <Link to={navigationRoutes.main.path} className="flex items-center gap-2">
+          <span className="sm:inline">Back to {navigationRoutes.main.title}</span>
+        </Link>
+      </Button>
+    </div>
+  );
+};
--- src/features/fetch-products/components/productList.tsx
+++ src/features/fetch-products/components/productList.tsx
@@ -1,5 +1,6 @@
 import { DataErrorElement } from '~components/ui/data-error-element/dataErrorElement';
 import { Spinner } from '~components/ui/spinner/spinner';
+import { EmptyList } from '~features/fetch-products/components/emptyList';
 import { ProductItem } from '~features/fetch-products/components/productItem';
 import { useProductData } from '~features/fetch-products/hooks/useProductData';
 import { withDataTestId } from '~utils/helpers';
@@ -11,14 +12,16 @@ export const ProductList = () => {
   if (error) return <DataErrorElement errorText={normalizeError(error).message} retryAction={refresh} />;
   if (isLongLoading) return <Spinner className="m-auto" size="xl" />;
 
-  return (
+  return products ? (
     <ul
       className="m-2 grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 lg:grid-cols-3"
       {...withDataTestId('catalog-page-product-list')}
     >
-      {products?.map((product) => {
+      {products.map((product) => {
         return <ProductItem product={product} key={product.id} />;
       })}
     </ul>
+  ) : (
+    <EmptyList />
   );
 };
