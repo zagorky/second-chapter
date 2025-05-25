de0ba30b65a75c5d6a8e9172f1984396de8e21ba
zagorky <zagorkiynahyi@gmail.com>
2025-05-25 17:28:54 +0300

    feat: start divide categories and subcategories

--- src/features/categories/utils/buildCategoriesMap.ts
+++ src/features/categories/utils/buildCategoriesMap.ts
@@ -2,8 +2,14 @@ import type { ProductProjection } from '@commercetools/platform-sdk';
 
 import { DEFAULT_STORE_LANGUAGE } from '~features/fetch-products/config/constants';
 
+type CategoryInfo = {
+  name: string;
+  count: number;
+  parentId?: string;
+};
+
 export const buildCategoriesMap = (products: ProductProjection[]) => {
-  const categoriesMap = new Map<string, { name: string; count: number }>();
+  const categoriesMap = new Map<string, CategoryInfo>();
 
   products.forEach((product) => {
     product.categories.forEach((categoryReference) => {
@@ -15,12 +21,14 @@ export const buildCategoriesMap = (products: ProductProjection[]) => {
 
       const id = category.key;
       const name = category.name[DEFAULT_STORE_LANGUAGE];
+      const parentId = category.parent?.obj?.key;
+
       const existingCategory = categoriesMap.get(id);
 
       if (existingCategory) {
         existingCategory.count += 1;
       } else {
-        categoriesMap.set(id, { name, count: 1 });
+        categoriesMap.set(id, { name, count: 1, parentId });
       }
     });
   });
