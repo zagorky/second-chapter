f9e8554021ef730186fd7cef7911bfed6bd1396b
zagorky <zagorkiynahyi@gmail.com>
2025-05-26 20:05:45 +0300

    fix: fix error in logic but not sure

--- src/features/filters/utils/buildCategoryQueryParameters.ts
+++ src/features/filters/utils/buildCategoryQueryParameters.ts
@@ -1,6 +1,7 @@
 export const buildCategoryQueryParameters = (subcategory: string, category?: string) => {
   if (!category && !subcategory) return {};
   if (category && subcategory) return { filter: [`categories.id:"${category}"`, `categories.id:"${subcategory}"`] };
+  if (category) return { filter: [`categories.id:"${category}"`] };
 
   return { filter: [`categories.id:"${subcategory}"`] };
 };
