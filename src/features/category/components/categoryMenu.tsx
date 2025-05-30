019884db62d676739e82dba2bed443db7f2d9b17
zagorky <zagorkiynahyi@gmail.com>
2025-05-30 13:47:31 +0300

    feat: add style to a category menu

--- src/features/category/components/categoryMenu.tsx
+++ src/features/category/components/categoryMenu.tsx
@@ -1,3 +1,5 @@
+import type { CategoryNode } from '~features/category/utils/buildCategories';
+
 import { useCategoryData } from '~features/category/hooks/useCategoryData';
 import { useSearchParams } from 'react-router';
 
@@ -7,6 +9,7 @@ import {
   DrawerClose,
   DrawerContent,
   DrawerDescription,
+  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -16,65 +19,90 @@ export const CategoryMenu = () => {
   const { categories } = useCategoryData();
 
   const [searchParameters, setSearchParameters] = useSearchParams();
-  const currentCategory = searchParameters.get('category')
-    ? categories.find((category) => category.id === searchParameters.get('category'))?.name
-    : '';
-  const currentSubcategory = searchParameters.get('subcategory')
-    ? categories.find((category) => category.id === searchParameters.get('subcategory'))?.name
-    : '';
 
-  const handleClick = (categoryID: string) => {
+  const handleClick = (category: CategoryNode) => {
     const newParameter = new URLSearchParams(searchParameters);
-    const selectedCategory = categories.find((category) => category.id === categoryID);
-    const isSubcategory = selectedCategory?.parentCategory !== '';
 
-    if (isSubcategory) {
-      newParameter.set('subcategory', categoryID);
-    } else {
-      newParameter.set('category', categoryID);
+    if (category.parent === '') {
+      newParameter.set('category', category.id);
       newParameter.delete('subcategory');
+    } else {
+      newParameter.set('category', category.parent);
+      newParameter.set('subcategory', category.id);
     }
 
     setSearchParameters(newParameter);
   };
 
-  const renderHeader = () => {
-    if (currentSubcategory) {
-      return currentSubcategory;
-    } else if (currentCategory) {
-      return currentCategory;
-    } else {
-      return 'Categories';
-    }
+  const handleClickAll = () => {
+    const newParameter = new URLSearchParams(searchParameters);
+
+    newParameter.delete('category');
+    newParameter.delete('subcategory');
+    setSearchParameters(newParameter);
   };
 
   return (
     <Drawer direction="right">
       <DrawerTrigger asChild>
-        <Button className="capitalize">{renderHeader()}</Button>
+        <Button className="capitalize">Categories</Button>
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>Genres</DrawerTitle>
+          <DrawerTitle className="text-2xl">Genres</DrawerTitle>
           <DrawerDescription className="sr-only">Genres</DrawerDescription>
         </DrawerHeader>
-        <ul className="overflow-y-auto px-4 text-sm">
+        <ul className="overflow-y-auto px-4 text-left text-sm">
+          <li key="all-books">
+            <DrawerClose asChild>
+              <Button
+                className="hover:bg-main/60 mb-4 w-full cursor-pointer justify-start text-lg leading-normal font-bold capitalize"
+                onClick={handleClickAll}
+                variant="ghost"
+              >
+                All Books
+              </Button>
+            </DrawerClose>
+          </li>
           {categories.map((category) => (
-            <li key={category.name}>
-              <DrawerClose asChild>
-                <Button
-                  className="hover:bg-secondary-background/60 mb-4 w-full cursor-pointer text-lg leading-normal capitalize"
-                  onClick={() => {
-                    handleClick(category.id);
-                  }}
-                  variant="ghost"
-                >
-                  {category.name}
-                </Button>
-              </DrawerClose>
-            </li>
+            <div key={category.id}>
+              <li>
+                <DrawerClose asChild>
+                  <Button
+                    className="hover:bg-main/60 mb-4 w-full cursor-pointer justify-start text-lg leading-normal font-bold capitalize"
+                    onClick={() => {
+                      handleClick(category);
+                    }}
+                    variant="ghost"
+                  >
+                    {category.name}
+                  </Button>
+                </DrawerClose>
+              </li>
+              {category.children.length > 0 &&
+                category.children.map((child) => (
+                  <li key={child.id} className="ml-4">
+                    <DrawerClose asChild>
+                      <Button
+                        className="hover:bg-main/60 text-ьв mb-4 w-full cursor-pointer justify-start leading-normal capitalize"
+                        onClick={() => {
+                          handleClick(child);
+                        }}
+                        variant="ghost"
+                      >
+                        {child.name}
+                      </Button>
+                    </DrawerClose>
+                  </li>
+                ))}
+            </div>
           ))}
         </ul>
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant="neutral">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
--- src/features/category/hooks/useCategoryData.ts
+++ src/features/category/hooks/useCategoryData.ts
@@ -1,27 +1,18 @@
 import type { Category } from '@commercetools/platform-sdk';
 import type { FetchProductsParameters } from '~features/fetch-products/utils/fetchProducts';
 
+import { buildCategoryTree } from '~features/category/utils/buildCategories';
 import { fetchCategory } from '~features/category/utils/fetchCategory';
-import { DEFAULT_STORE_LANGUAGE } from '~features/fetch-products/config/constants';
 import useSWR from 'swr';
 
 export const useCategoryData = (parameters?: FetchProductsParameters) => {
   const actualParameters = { expand: ['category[*]', 'ancestors[*]'], limit: 100, ...parameters };
 
   const { data: categories } = useSWR<Category[], Error>(actualParameters, fetchCategory);
-  const categoryObjects = categories?.map((cat) => {
-    const parentCategory = cat.ancestors[0]?.obj?.name[DEFAULT_STORE_LANGUAGE] ?? '';
-    const name = cat.name[DEFAULT_STORE_LANGUAGE];
-    const id = cat.id;
 
-    return {
-      id,
-      parentCategory,
-      name,
-    };
-  });
+  const categoriesTree = buildCategoryTree(categories ?? []);
 
   return {
-    categories: categoryObjects ?? [],
+    categories: categoriesTree,
   };
 };
