5db21f35b4abd0c186e2d0b91b59687dc9eedced
zagorky <zagorkiynahyi@gmail.com>
2025-07-01 13:22:11 +0300

    feat: add main page skeleton

---
+++ src/components/ui/page-skeleton/mainPageSkeleton.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from '../skeleton';
+
+export const MainPageSkeleton = () => {
+  return (
+    <div className="space-y-12">
+      <div className="flex flex-col-reverse gap-6 p-6 lg:grid lg:grid-cols-[2fr_1fr] lg:gap-12">
+        <div>
+          <Skeleton className="mb-2 h-28 w-full max-w-xl" />
+          <Skeleton className="mb-6 h-14 w-full max-w-sm" />
+          <Skeleton className="mb-7 h-16 w-full max-w-50" />
+          <Skeleton className="h-12 w-full max-w-42" />
+        </div>
+        <div className="flex justify-center">
+          <Skeleton className="h-84 w-98" />
+        </div>
+      </div>
+
+      <div className="space-y-4">
+        <Skeleton className="mx-auto h-76 w-full max-w-7xl" />
+        <div className="flex justify-center gap-2"></div>
+      </div>
+    </div>
+  );
+};
--- src/components/ui/page-skeleton/pageSkeleton.tsx
+++ src/components/ui/page-skeleton/pageSkeleton.tsx
@@ -1,22 +1,54 @@
-import { Skeleton } from '~components/ui/skeleton';
+import { MainPageSkeleton } from '~components/ui/page-skeleton/mainPageSkeleton';
+import { navigationRoutes } from '~config/navigation';
 import { useTimeout } from '~hooks/useTimeout';
 import { useState } from 'react';
+import { useMatches } from 'react-router';
 
 const VISIBILITY_DELAY = 1000;
 
 export const PageSkeleton = () => {
-  const [isVisible, setVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const matches = useMatches();
 
   useTimeout(() => {
-    setVisible(true);
+    setIsVisible(true);
   }, VISIBILITY_DELAY);
 
-  return (
-    isVisible && (
-      <div className="flex w-full flex-col items-center gap-4 p-4">
-        <Skeleton className="h-8 w-3/4 rounded-md sm:w-1/2" />
-        <Skeleton className="h-40 w-3/5 rounded-lg sm:w-1/2" />
-      </div>
-    )
-  );
+  return isVisible && matchPath(matches[1].pathname);
 };
+
+function matchPath(pathname: string) {
+  switch (pathname) {
+    case navigationRoutes.about.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.catalog.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.cart.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.login.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.signup.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.profile.path: {
+      return <></>;
+    }
+
+    case navigationRoutes.product.path: {
+      return <></>;
+    }
+
+    default: {
+      return <MainPageSkeleton />;
+    }
+  }
+}
