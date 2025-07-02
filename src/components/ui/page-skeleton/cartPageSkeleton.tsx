4a05f097bb1209f51e29c91f8d70a9ab9de2cf33
zagorky <zagorkiynahyi@gmail.com>
2025-07-02 16:46:26 +0300

    feat: add cart page skeleton

--- src/components/ui/page-skeleton/cartPageSkeleton.tsx
+++ src/components/ui/page-skeleton/cartPageSkeleton.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from '~components/ui/skeleton';
 import { VISIBILITY_DELAY } from '~config/constants';
 import { useTimeout } from '~hooks/useTimeout';
 import { useState } from 'react';
@@ -9,5 +10,31 @@ export const CartPageSkeleton = () => {
     setIsVisible(true);
   }, VISIBILITY_DELAY);
 
-  return isVisible && <></>;
+  return (
+    isVisible && (
+      <div className="flex flex-grow flex-col justify-between gap-8">
+        <div className="grid gap-4">
+          <div className="flex justify-end">
+            <Skeleton className="h-10 w-full max-w-23" />
+          </div>
+          <ul className="grid gap-4">
+            <Skeleton className="bg-background relative flex h-90 justify-between gap-4 p-4 sm:h-48" />
+          </ul>
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 w-full" />
+          <Skeleton className="bg-main h-10 w-full max-w-19" />
+        </div>
+
+        <div className="grid gap-6">
+          <Skeleton className="h-10 w-full max-w-36 items-start" />
+          <div className="space-y-3">
+            <Skeleton className="relative flex h-46 justify-between gap-4 p-4" />
+          </div>
+          <Skeleton className="bg-main h-10 w-full max-w-34 justify-self-end" />
+        </div>
+      </div>
+    )
+  );
 };
