950d83aacf5391529891cb3a6c2b1da65177bcf7
zagorky <zagorkiynahyi@gmail.com>
2025-07-02 17:45:28 +0300

    refactor: refactor catalog page skeleton

--- src/components/ui/page-skeleton/catalogPageSkeleton.tsx
+++ src/components/ui/page-skeleton/catalogPageSkeleton.tsx
@@ -20,19 +20,19 @@ export const CatalogPageSkeleton = () => {
 
         <div className="my-4 flex flex-col justify-center gap-8 md:flex-row md:justify-between">
           <div className="flex w-full flex-2 gap-2">
-            <Skeleton className="w-full" />
-            <Skeleton className="bg-main w-15" />
-            <Skeleton className="bg-main w-15" />
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="bg-main h-10 w-15" />
+            <Skeleton className="bg-main h-10 w-15" />
           </div>
-          <Skeleton className="bg-main flex h-10 w-full max-w-xl flex-1" />
+          <Skeleton className="bg-main flex h-10 min-h-10 w-full flex-1" />
         </div>
 
         <div className="grid flex-col gap-4 lg:grid-cols-[minmax(230px,_250px)_1fr]">
           <Skeleton className="flex h-90 flex-col items-center gap-8" />
           <div className="flex flex-col items-center gap-8">
-            <ul className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+            <ul className="grid w-fit grid-cols-1 justify-center justify-items-center gap-8 md:grid-cols-2 xl:grid-cols-3">
               {Array.from({ length: ITEMS_PER_PAGE }).map((_, index) => (
-                <Skeleton key={index} className="h-144 w-full max-w-[300px] justify-self-center rounded-lg" />
+                <Skeleton key={index} className="h-144 w-75 justify-self-center rounded-lg" />
               ))}
             </ul>
             <Skeleton className="bg-main my-6 h-12 w-full max-w-sm" />
