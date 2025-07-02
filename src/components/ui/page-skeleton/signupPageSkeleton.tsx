19fa39ab1a832131a62ec468b12ea9e93745d7f2
zagorky <zagorkiynahyi@gmail.com>
2025-07-02 18:55:36 +0300

    refactor: refactor singup page skeleton

--- src/components/ui/page-skeleton/signupPageSkeleton.tsx
+++ src/components/ui/page-skeleton/signupPageSkeleton.tsx
@@ -15,8 +15,8 @@ export const SignupPageSkeleton = () => {
       <div className="flex flex-col items-center gap-2">
         <Skeleton className="h-12 w-full max-w-36" />
         <div className="flex w-[calc(100%-32px)] max-w-full flex-col gap-6">
-          <Skeleton className="bg-background flex h-[920px] w-full items-end justify-center">
-            <Skeleton className="bg-main mb-16 h-10 w-full max-w-283" />
+          <Skeleton className="bg-background flex h-[1580px] w-full items-end justify-center md:h-[922px]">
+            <Skeleton className="bg-main mx-8 mb-16 h-10 w-full max-w-283" />
           </Skeleton>
         </div>
       </div>
