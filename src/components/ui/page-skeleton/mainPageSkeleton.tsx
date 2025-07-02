f86324c99dcc762eb5690ba3673d9a82b7c52d1c
zagorky <zagorkiynahyi@gmail.com>
2025-07-02 17:07:44 +0300

    refactor: refactor main page skeleton

--- src/components/ui/page-skeleton/mainPageSkeleton.tsx
+++ src/components/ui/page-skeleton/mainPageSkeleton.tsx
@@ -14,17 +14,20 @@ export const MainPageSkeleton = () => {
   return (
     isVisible && (
       <div className="space-y-12">
-        <div className="flex flex-col-reverse gap-6 p-6 lg:grid lg:grid-cols-[2fr_1fr] lg:gap-12">
-          <div>
-            <Skeleton className="mb-2 h-28 w-full max-w-xl" />
-            <Skeleton className="mb-6 h-14 w-full max-w-sm" />
-            <Skeleton className="mb-7 h-16 w-full max-w-50" />
-            <Skeleton className="bg-main h-12 w-full max-w-42" />
+        <section className="relative flex flex-col-reverse gap-1 bg-contain bg-right bg-no-repeat px-6 text-left lg:grid lg:grid-cols-[2fr_1fr] lg:py-6">
+          <div className="space-y-6">
+            <div className="space-y-4 text-left">
+              <Skeleton className="h-28 w-full max-w-xl" />
+              <Skeleton className="h-14 w-full max-w-sm" />
+            </div>
+            <Skeleton className="h-16 w-full max-w-50" />
+            <Skeleton className="bg-main h-12 w-40" />
           </div>
-          <div className="flex justify-center">
-            <Skeleton className="h-84 w-98" />
+
+          <div className="relative flex max-w-[200px] flex-col items-end pt-[50px] sm:max-w-[300px] sm:pt-[100px]">
+            <Skeleton className="aspect-[1/1] w-full rounded-lg" />
           </div>
-        </div>
+        </section>
 
         <div className="space-y-4">
           <Skeleton className="mx-auto h-76 w-full max-w-7xl" />
