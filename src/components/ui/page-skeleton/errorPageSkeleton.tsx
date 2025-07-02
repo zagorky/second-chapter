cb965a02e53a2800fb32c619a8f7af2d641f0449
zagorky <zagorkiynahyi@gmail.com>
2025-07-02 18:18:04 +0300

    feat: add error page skeleton

--- src/components/ui/page-skeleton/errorPageSkeleton.tsx
+++ src/components/ui/page-skeleton/errorPageSkeleton.tsx
@@ -15,7 +15,7 @@ export const ErrorPageSkeleton = () => {
     isVisible && (
       <div className="m-auto flex h-[calc(100vh-200px)] w-full max-w-xl flex-col justify-center p-10">
         <div className="flex w-full flex-col gap-4 p-4">
-          <Skeleton className="h-70 w-full max-w-xl py-5" />
+          <Skeleton className="h-60 w-full max-w-xl py-5" />
           <Skeleton className="bg-main m-auto h-10 w-full max-w-30" />
         </div>
       </div>
